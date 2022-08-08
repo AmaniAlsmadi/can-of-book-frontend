f085a2ec804126cb82cf9a7c3548eeb90fb531e4
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-08-09 00:24:57 +0300

    add auth0 1

--- src/App.js
+++ src/App.js
@@ -3,31 +3,41 @@ import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './component/profile';
+import About from './About';
+//import Welcome from './Welcome';
+
+//import { useAuth0 } from '@auth0/auth0-react';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
-          <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
-          </Routes>
-          <Footer />
-        </Router>
-      </>
-    )
-  }
+export default function App() {
+
+  //const { isAuthenticated } = useAuth0();
+
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            exact path="/"
+            element={<BestBooks />}
+          >
+          </Route>
+          <Route path="/" element={<App />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
+
+ 
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+
